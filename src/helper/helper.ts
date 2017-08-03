792f96365eadbabcd1b6040ccf8282ee0097793e
Yohei Shibuya <yoshibu.dvelop@gmail.com>
2017-08-04 01:03:10 +0900

    add defaultSpy function

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -1,7 +1,15 @@
+export interface IDefaultSpy {
+    f: () => any;
+}
+
 export function spyOnAll<T>(obj: T): T {
     for (let key in obj)
         spyOn(obj, key);
     return obj;
+}
+
+export function defaultSpy(): IDefaultSpy {
+    return spyOnAll<IDefaultSpy>({ f() {} });
 }
 
 export function add(x: number, y: number): number {
