a9628401c09a4f148bc635e3148be1864b8d06da
Yohei Shibuya <yoshibu.dvelop@gmail.com>
2017-08-03 15:56:48 +0900

    add browser output

--- bundle.js
+++ bundle.js
@@ -1,12 +1,14 @@
 const { rollup } = require("rollup");
 const tsp = require("rollup-plugin-typescript");
+const uglify = require("rollup-plugin-uglify");
 const typescript = require("typescript");
 
 const path = require("path");
+const entry = path.resolve("./src/index.ts");
 
 
 rollup({
-    entry: path.resolve("./src/index.ts"),
+    entry,
     plugins: [
         tsp({ typescript })
     ]
@@ -16,9 +18,24 @@ rollup({
         sourceMap: true,
         format: "cjs"
     });
-
     val.write({
         dest: path.resolve("./dist/soboku.mjs"),
         format: "es"
     });
 });
+
+
+rollup({
+    entry,
+    plugins: [
+        tsp({ typescript, target: "ES3" }),
+        uglify()
+    ],
+}).then(val => {
+    val.write({
+        dest: path.resolve("./dist/soboku.browser.min.js"),
+        format: "iife",
+        moduleName: "soboku",
+        sourceMap: true
+    });
+})
