bd56082567806f375f98c0be5277a0dd30ba736d
Yohei Shibuya <yoshibu.dvelop@gmail.com>
2017-08-05 22:11:09 +0900

    change some interface

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 export type Listener<T> = (val: T) => void;
-export interface IUnListener {
-    unlisten(): void;
+export interface IUnsubscriber {
+    unsubscribe(): void;
 }
 export interface IReporter<T> {
-    report(listener: Listener<T>, thisArg?: any): IUnListener;
+    report(listener: Listener<T>, thisArg?: any): IUnsubscriber;
     listenerCount(): number;
 }
 
@@ -15,7 +15,7 @@ export interface IStateHolder<T> {
     s(): T;
 }
 
-export interface ISobokuArray<T> extends IReporter<T[]> {
+export interface ISArray<T> extends IReporter<T[]> {
     s(): T[];
     pop(): T | undefined;
     push(...items: T[]): number;
@@ -35,24 +35,25 @@ export type Atom<T> = T | Calc<T>;
 export function reporter<T>(): Reporter<T>;
 export function gate<T>(gatekeeper: IStateHolder<boolean>, reporter: IReporter<T>): IReporter<T>;
 export function state<T>(initial: T): State<T>;
+export function sarray<T>(initial?: T[]): ISArray<T>;
 export function combine<T>(source: { [K in keyof T]: Atom<T[K]>}): Calc<T>;
-export function dependency<R, A1>(func: (arg1: A1) => R, a1: Atom<A1>): Calc<R>;
-export function dependency<R, A1, A2>(func: (arg1: A1, arg2: A2) => R, a1: Atom<A1>, a2: Atom<A2>): Calc<R>;
-export function dependency<R, A1, A2, A3>(func: (arg1: A1, arg2: A2, arg3: A3) => R, a1: Atom<A1>, a2: Atom<A2>, a3: Atom<A3>): Calc<R>;
-export function dependency<R, A1, A2, A3, A4>(func: (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => R, a1: Atom<A1>, a2: Atom<A2>, a3: Atom<A3>, a4: Atom<A4>): Calc<R>;
-export function dependency<R>(func: (...args: any[]) => R, ...states: Atom<any>[]): Calc<R>
-export function publisher<T>(permition: IReporter<boolean>, reporter: IReporter<T>): Calc<T>;
-export function trigger<A1>(func: (arg1: A1) => boolean, a1: Atom<A1>): Calc<true>;
-export function trigger<A1, A2>(func: (arg1: A1, arg2: A2) => boolean, a1: Atom<A1>, a2: Atom<A2>): Calc<true>;
-export function trigger<A1, A2, A3>(func: (arg1: A1, arg2: A2, arg3: A3) => boolean, a1: Atom<A1>, a2: Atom<A2>, a3: Atom<A3>): Calc<true>;
-export function trigger<A1, A2, A3, A4>(func: (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => boolean, a1: Atom<A1>, a2: Atom<A2>, a3: Atom<A3>, a4: Atom<A4>): Calc<true>;
-export function trigger(func: (...args: any[]) => boolean, ...states: Atom<any>[]): Calc<true>
+export function dependency<T, A1>(func: (arg1: A1) => T, a1: Atom<A1>): Calc<T>;
+export function dependency<T, A1, A2>(func: (arg1: A1, arg2: A2) => T, a1: Atom<A1>, a2: Atom<A2>): Calc<T>;
+export function dependency<T, A1, A2, A3>(func: (arg1: A1, arg2: A2, arg3: A3) => T, a1: Atom<A1>, a2: Atom<A2>, a3: Atom<A3>): Calc<T>;
+export function dependency<T, A1, A2, A3, A4>(func: (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => T, a1: Atom<A1>, a2: Atom<A2>, a3: Atom<A3>, a4: Atom<A4>): Calc<T>;
+export function dependency<T>(func: (...args: any[]) => T, ...states: Atom<any>[]): Calc<T>
+export function publisher<T>(permition: Calc<boolean>, reporter: Calc<T>): Calc<T>;
+export function trigger<A1>(func: (arg1: A1) => boolean, a1: Atom<A1>): Calc<boolean>;
+export function trigger<A1, A2>(func: (arg1: A1, arg2: A2) => boolean, a1: Atom<A1>, a2: Atom<A2>): Calc<boolean>;
+export function trigger<A1, A2, A3>(func: (arg1: A1, arg2: A2, arg3: A3) => boolean, a1: Atom<A1>, a2: Atom<A2>, a3: Atom<A3>): Calc<boolean>;
+export function trigger<A1, A2, A3, A4>(func: (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => boolean, a1: Atom<A1>, a2: Atom<A2>, a3: Atom<A3>, a4: Atom<A4>): Calc<boolean>;
+export function trigger(func: (...args: any[]) => boolean, ...states: Atom<any>[]): Calc<boolean>
 
-export interface SObservable<I extends Reporter<any>, O> {
+export interface ISObservable<I extends Reporter<any>, O> {
     readonly input: I;
     readonly output: IReporter<O>;
 }
 
-export function interval(ms: Atom<number>): SObservable<State<boolean>, number>;
-export function timeout(ms: Atom<number>): SObservable<State<boolean>, number>;
-export function sequenceEqual<T>(sequence: T[], compareFn?: (x: T, y: T) => boolean): SObservable<Reporter<T>, true>;
+export function interval(ms: Atom<number>): ISObservable<State<boolean>, number>;
+export function timeout(ms: Atom<number>): ISObservable<State<boolean>, number>;
+export function sequenceEqual<T>(sequence: T[]  | ISArray<T>, compareFn?: (x: T, y: T) => boolean): ISObservable<Reporter<T>, true>;
