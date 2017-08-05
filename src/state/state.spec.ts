02ba3f9b20b5f38b07f211c6d0d0a6373fd60350
Yohei Shibuya <yoshibu.dvelop@gmail.com>
2017-08-05 10:24:14 +0900

    remove dead describe

--- src/state/state.spec.ts
+++ src/state/state.spec.ts
@@ -3,28 +3,25 @@ import { ISobokuArray } from "../../index.d";
 import { IDefaultSpy, defaultSpy } from "../helper/helper";
 
 
-describe("soboku", () => {
+describe("state", () => {
     let r: IDefaultSpy;
     beforeEach(() => r = defaultSpy());
-    
-    describe("state", () => {
-        it("should have state", () => {
-            const num = state(100);
 
-            expect(num.s()).toBe(100);
+    it("should have state", () => {
+        const num = state(100);
 
-            num.next(20);
+        expect(num.s()).toBe(100);
 
-            expect(num.s()).toBe(20);
-        });
-        it("should emit listeners when next emitted", () => {
-            const num = state(20);
-            num.report(r.f);
-            num.next(100);
+        num.next(20);
 
-            expect(r.f).toHaveBeenCalledTimes(1);
-            expect(r.f).toHaveBeenCalledWith(100);
-        });
+        expect(num.s()).toBe(20);
     });
+    it("should emit listeners when next emitted", () => {
+        const num = state(20);
+        num.report(r.f);
+        num.next(100);
 
+        expect(r.f).toHaveBeenCalledTimes(1);
+        expect(r.f).toHaveBeenCalledWith(100);
+    });
 });
